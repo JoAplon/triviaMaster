dfb2b71ec8ef6c56d8ed47f1911a9b54f7d76055
JoAplon <158791447+JoAplon@users.noreply.github.com>
2024-04-26 10:23:28 -0700

    added login/signup button

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,11 +8,34 @@ import '../css/header.css'
 const Header = () => {
     const location = useLocation();
     const [showTooltip, setShowTooltip] = useState(location.pathname === '/');
+    const [showTooltipButton, setShowTooltipButton] = useState(location.pathname === '/');
+    const [showProfileMenu, setShowProfileMenu] = useState(false);
+    
+    const toggleProfileMenu = () => {
+        setShowProfileMenu(!showProfileMenu);
+    };
+
 
     useEffect(() => {
-        console.log('Current pathname:', location.pathname);
+        // console.log('Current pathname:', location.pathname);
         setShowTooltip(location.pathname === '/');
+        setShowTooltipButton(location.pathname === '/');
     }, [location]);
+
+    const ProfileMenu = () => {
+        return (
+            <div className="profileMenu">
+                <ul>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                    <li>
+                        <Link to="/signup">Sign Up</Link>
+                    </li>
+                </ul>
+            </div>
+        );
+    };
 
     return (
         <header className="header">
@@ -21,9 +44,11 @@ const Header = () => {
                     {/* tooltip button dissapears when you leave the page
                      but you need to refresh first. will come back to it later. */}
                     <li className='directions'>
+                        {showTooltipButton && (
                         <button className='directionsButton'>
                             <img src='https://img.icons8.com/pulsar-line/48/ask-question.png'/>
                         </button>
+                        )}
                         {showTooltip && (
                             <div className='directionsTooltip'>
                                 You can play without making an account.
@@ -32,11 +57,12 @@ const Header = () => {
                             </div>
                         )}
                     </li>
-                    <Link to="profile">
-                        <button className='profileButton'>
+                    <li className="profile">
+                        <button className='profileButton'  onClick={toggleProfileMenu}>
                         <img src='https://img.icons8.com/wired/64/test-account.png'/>
                         </button>
-                    </Link>
+                        {showProfileMenu && <ProfileMenu />}
+                    </li>
 
                 </ul>
             </nav>
